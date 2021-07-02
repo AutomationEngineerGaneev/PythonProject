851b79908d38116804a54438ededbf8deee64155
artur <a>
2021-07-02 20:51:52 +0300

    Arrays.

---
+++ example.py
@@ -0,0 +1,28 @@
+a = 1 / 3
+a = a + 2
+
+print(a)
+print(type(a))
+print(a + a + a)
+
+b_true = True
+b_false = False
+
+if b_true:
+    print('Okey')
+else:
+    print('Not Okey')
+
+n = None
+
+s='1234567890'
+print(type(s))
+s= 'Hello' + 'World'
+
+s=f'Value of {a=}'
+print(s)
+
+
+print(s[0])
+print(s[1:5]) #slice
+print(s[10:-1]) #size of str -1 symbol

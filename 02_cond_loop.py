5894d7f66c38e0790a36b38494b28d6a54ae465a
artur <a>
2021-07-03 10:01:13 +0300

    'For' loop.

--- 02_cond_loop.py
+++ 02_cond_loop.py
@@ -5,13 +5,27 @@ elif 8 / 3 == 2.66666666:
 else:
     print('Not ok')
 
-password = ''  # not known
+# password = ''  # not known
+password = '5432'  # not known
 
 if not password:
     true_password = '12345'
 else:
     true_password = password
 
-true_password = password if  password else '12345'
+true_password = password if password else '12345'
 print(true_password)
 
+# for i in 1: так делать нельзя , нужно ходить по спискам или словарям,строкам
+# for i in [1, 2, 3, 4, 5, 6]:
+# for i in range(10):
+for i in range(1, 11):
+    print(i * i)
+# Четные числа попадают
+for i in range(1, 11):
+    if i % 2 == 0:
+        print(i * i)
+
+    # List Comprehension
+lc = [i * i for i in range(1, 11) if i % 2 == 0]
+print(lc)
